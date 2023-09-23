765a953a595c2ae85554eeba62d7616fb8be1d8b
Vladimir <fruityflunt@gmail.com>
2023-09-23 07:37:06 +0300

    Pagination - view

--- client/src/components/ProductsList/ProductsList.tsx
+++ client/src/components/ProductsList/ProductsList.tsx
@@ -1,23 +1,37 @@
 import { FC } from "react";
 import "./ProductsList.scss";
 import ProductMedium from "../Product/ProductMedium/ProductMedium";
+import Button from "../UI/Button/Button";
 
 const ProductsList: FC = () => {
   return (
-    <div className="product-list">
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-      <ProductMedium />
-    </div>
+    <>
+      <div className="product-list">
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+        <ProductMedium />
+      </div>
+      <div className="pagination">
+        <Button className="pagination__button" isInverted={true}>
+          1
+        </Button>
+        <Button className="pagination__button" isInverted={true}>
+          2
+        </Button>
+        <Button className="pagination__button" isInverted={true}>
+          3
+        </Button>
+      </div>
+    </>
   );
 };
 
--- client/src/components/UI/Button/Button.tsx
+++ client/src/components/UI/Button/Button.tsx
@@ -1,10 +1,19 @@
 import { ButtonHTMLAttributes, FC } from "react";
 import "./Button.scss";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isInverted?: boolean;
+}
 
-const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
-  const finalClassName = className ? `button ${className}` : "button";
+const Button: FC<ButtonProps> = ({
+  children,
+  className,
+  isInverted = false,
+  ...props
+}) => {
+  const finalClassName = className
+    ? `button ${isInverted && "button--inverted"} ${className}`
+    : `button ${isInverted && "button--inverted"}`;
 
   return (
     <button className={finalClassName} {...props}>
